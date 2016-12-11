bb050efce9707e95384750426cd74d6a73b3b972
Ron Yu <ronyu77@gmail.com>
2016-12-11 19:37:18 +0800

    add app PATCH method test

--- server/server.js
+++ server/server.js
@@ -86,7 +86,7 @@ app.patch('/todos/:id', (req, res) => {
     body.completedAt = new Date().getTime();
   }else {
     body.completed = false;
-    body.completedAt = null;s
+    body.completedAt = null;
   }
 
   Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then(todo => {
